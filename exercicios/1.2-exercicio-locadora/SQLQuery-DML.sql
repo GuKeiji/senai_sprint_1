2b46c08bbba7e752ebe0044f8252b23fcdd49248
Gustavo Keiji <gugu.keiji.2004@gmail.com>
2021-08-05 18:40:53 -0300

    DDL, DML e DQL(1.2, 1.3, 1.4)

---
+++ exercicios/1.2-exercicio-locadora/SQLQuery-DML.sql
@@ -0,0 +1,20 @@
+USE LOCADORA;
+GO
+
+INSERT INTO EMPRESA (nomeEmpresa)
+VALUES ('Unidas'), ('Maestro');
+
+INSERT INTO MARCA (nomeMarca)
+VALUES ('VW'), ('GM');
+
+INSERT INTO CLIENTE (nomeCliente)
+VALUES ('Jeremias'), ('Augusto');
+
+INSERT INTO MODELO (idMarca, nomeModelo)
+VALUES (1,'Polo'), (2,'Celta');
+
+INSERT INTO VEICULO (idModelo, idEmpresa, placa)
+VALUES (2,1,'EEE'), (1,2,'AAA')
+
+INSERT INTO ALUGUEL (idVeiculo, idCliente, dataAluguel)
+VALUES (1,2,'03/08/2021'), (2,1,'04/08/2021');
